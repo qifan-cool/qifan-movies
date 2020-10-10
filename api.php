7965ee914be2298bc8051b3aad8e3aecfaa8d7c4
qifan-cool <67260867+qifan-cool@users.noreply.github.com>
2020-10-10 10:27:40 +0800

    initial commit

--- api.php
+++
@@ -1,5 +0,0 @@
-<meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" id="viewport" name="viewport">
-<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" bgcolor="#000000">
-<iframe src="http://yun.sooyisi.com/vip/?url=<?php echo $_GET['url']?>" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" allOwfullscreen="true" scrolling="no" height="100%" width="100%"></iframe>
-
-
